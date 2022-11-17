4a10c91619f0e1a348c342e0a47177f9da101c96
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2022-11-17 19:58:07 +0530

    Edit navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,27 @@
 import "./Navbar.css";
 
 export const Navbar = ({ active }) => {
-	console.log(active);
 	if (active === "dashboard") {
 		return (
 			<div className="navbar-container">
 				<div className="navbar-links">
 					<div className="dashboard-link active">Dashboard</div>
-					<div className="bills-link">Bills</div>
+					<div className="bills-link">
+						<a href="/bills">Bills</a>
+					</div>
 				</div>
 			</div>
 		);
 	} else if (active === "bills") {
-		<div className="navbar-container">
-			<div className="navbar-links">
-				<div className="dashboard-link">Dashboard</div>
-				<div className="dashboard-link active">Bills</div>
+		return (
+			<div className="navbar-container">
+				<div className="navbar-links">
+					<div className="dashboard-link">
+						<a href="/dashboard">Dashboard</a>
+					</div>
+					<div className="bills-link active">Bills</div>
+				</div>
 			</div>
-		</div>;
+		);
 	}
 };
