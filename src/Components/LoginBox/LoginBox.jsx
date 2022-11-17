1bf5f833a340de4b7703f0725fb610b59e88ad62
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2022-11-17 19:59:33 +0530

    Edit login page

--- src/Components/LoginBox/LoginBox.jsx
+++ src/Components/LoginBox/LoginBox.jsx
@@ -6,12 +6,22 @@ export const LoginBox = () => {
 			<div className="loginbox-header">Welcome! Log in below!</div>
 			<form action="dashboard" method="" id="loginbox-form">
 				<div className="form-email">
-					<label for="email">E-mail:</label>
-					<input type="text" id="email" value="email@domain.com" />
+					<label htmlFor="email">E-mail:</label>
+					<input
+						type="text"
+						id="email-field"
+						value="email@domain.com"
+						className="login-field"
+					/>
 				</div>
 				<div className="form-password">
-					<label for="password">Password:</label>
-					<input type="password" id="password" value="hello123" />
+					<label htmlFor="password">Password:</label>
+					<input
+						type="password"
+						id="password-field"
+						value="hello123"
+						className="login-field"
+					/>
 				</div>
 				<button className="form-button">Login</button>
 			</form>
